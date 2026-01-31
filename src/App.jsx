6cbbaa7260df3ad3bddbea06d65a8df7e213ac34
Kishan Kumar <kkishankumar9983@gmail.com>
2026-02-01 03:47:31 +0530

    Add some animation, fonts, videos and images

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import gsap from "gsap";
 import { ScrollTrigger, SplitText }  from "gsap/all" ;
 import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className='h-dvh bg-black'></div>
     </main>
   )
 }
