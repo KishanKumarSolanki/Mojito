3e7abd11f4c49adf79f60e5b20421ae539bb95f4
Kishan Kumar <kkishankumar9983@gmail.com>
2026-02-02 03:55:11 +0530

    Add about section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ScrollTrigger, SplitText }  from "gsap/all" ;
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
 import Cocktails from './Components/Cocktails';
+import About from './Components/About';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   )
 }
